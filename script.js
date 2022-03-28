77809088083822f29a5ef50ff38f1219cc95c5ae
Monique Levey <monique.w.91@outlook.com>
2022-03-28 21:37:15 +0800

    finished daily weather card

--- script.js
+++ script.js
@@ -1,33 +1,70 @@
+const formSearch = document.getElementById('form-search');
+const inputCity = document.getElementById('input-city');
+const h2CityToday = document.getElementById("h2-city-today");
+const spanDateToday = document.getElementById("span-date-today");
+const spantodayTemp = document.getElementById("span-today-temp");
+const spantodayWind = document.getElementById("span-today-wind");
+const spantodayHumidity = document.getElementById("span-today-humidity");
+const spantodayUv = document.getElementById("span-today-uv");
+const weatherCards = document.getElementById("weather-cards");
+
+
+
+
+
 const weatherApiKey = "2ae743b4bdb682278613148289f35d91";
+
+let today = new Date();
+let dd = String(today.getDate()).padStart(2, '0');
+let mm = String(today.getMonth() + 1).padStart(2, '0');
+let yyyy = today.getFullYear();
+
+today = dd + '-' + mm + '-' + yyyy;
+spanDateToday.textContent = today;
 
 
 // when the user inputs a city
 // then fetch data from weather api by city name
 function getCurrentWeatherApi(city){
     const url = 
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherApiKey}`;
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${weatherApiKey}`;
 
-    fetch(url).then(function(response){
+    return fetch(url).then(function(response){
         return response.json();
         console.log(response);
-    })
-    .then(function(res){
-        console.log(res)
     });
     
 }
 
+async function oneCall(lon, lat) {
+
+    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`
+
+       const response = await fetch(url);
+    return await response.json();
+}
+
+
+
 // retrieve latitude and longitude
-
+formSearch.addEventListener('submit', function(event){
+    event.preventDefault();
+    const city = inputCity.value;
+    getCurrentWeatherApi(city)
+    .then(function(data){
+        console.log('data is', data);
+        h2CityToday.textContent = city.toUpperCase();
+        spantodayWind.textContent = data.wind.speed;
+        spantodayTemp.textContent = data.main.temp;
+        spantodayHumidity.textContent = data.main.humidity;
+    });
+});
 // then call onecallAPI in celcius
-// function getOneCallApi(lat, lon) {
-//     const urlOneCallApi = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,daily&appid=${weatherApiKey}`;
-//     return fetch(urlOneCallApi).then(function(res) {
-//         return res.json();
-//     });
-// }
 
-getCurrentWeatherApi('tokyo');
+
+
+
+
 
 //         const lon = data.coord.lon;
 //         const lat = data.coord.lat;
