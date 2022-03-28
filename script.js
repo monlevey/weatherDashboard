e22b082c92b7019664fc84f6ff1b7eb4a445b951
Monique Levey <monique.w.91@outlook.com>
2022-03-28 15:09:30 +0800

    commented out js, css is mobile and desktop flex ready, html elements are complete

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 
 
 // when the user inputs a city
-// then fetch data from weather api
+// then fetch data from weather api by city name
+// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+// retrieve latitude and longitude
+
+// then call 
+// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 // data needed - 
 // for today
 // icon
 // temp
+    // convert from kelvin to celcius   
 // humidity
 // wind
+// get weather
 // uv
 // change UV colour depending on severity
 
@@ -20,4 +27,4 @@
 // change UV colour depending on severity
 
 // add city searched to local storage
-// display in a list of previously searched cities
+// display in a list of buttons of previously searched cities
