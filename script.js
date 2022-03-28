4e6bab11a95f44af1a42d45f5e8a755fa880a9c2
Monique Levey <monique.w.91@outlook.com>
2022-03-28 12:16:11 +0800

    commented out the js, created skeleton html, added files for css

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+// when the user inputs a city
+// then fetch data from weather api
+// data needed - 
+// for today
+// icon
+// temp
+// humidity
+// wind
+// uv
+// change UV colour depending on severity
+
+// for next 5 days
+// icon
+// temp
+// humidity
+// wind
+// uv
+// change UV colour depending on severity
+
+// add city searched to local storage
+// display in a list of previously searched cities
